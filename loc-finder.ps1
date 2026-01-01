8a1cf1ddba4b01743997501dd1c5b290cfcecbd4
Hangry-Studios <141448612+Hangry-Studios@users.noreply.github.com>
2026-01-01 13:19:45 +0100

    Update loc-finder.ps1

--- loc-finder.ps1
+++ loc-finder.ps1
@@ -1,10 +1,7 @@
 Add-Type -AssemblyName System.Device
-
 $Watcher = New-Object System.Device.Location.GeoCoordinateWatcher
-
 $Watcher.Start()
 
-Write-Host "Searching..." -ForegroundColor Cyan
 while (($Watcher.Status -ne 'Ready') -and ($Watcher.Permission -ne 'Denied')) {
     Start-Sleep -Milliseconds 100
 }
@@ -13,17 +10,26 @@ if ($Watcher.Permission -eq 'Denied') {
     Write-Warning "Access denied, check windows settings."
 } else {
     $Location = $Watcher.Position.Location
-
     if ($Location.IsUnknown) {
         Write-Warning "Could not determine location."
     } else {
+        $lat = $Location.Latitude.ToString().Replace(",", ".")
+        $lng = $Location.Longitude.ToString().Replace(",", ".")
+        
+        $url = "https://gps-coordinates.org/my-location.php?lat=$lat&lng=$lng"
+        
+        Start-Process $url
+        
         [PSCustomObject]@{
-            Latitud   = $Location.Latitude
-            Longitud  = $Location.Longitude
+            Latitud   = $lat
+            Longitud  = $lng
             Precision = "$($Location.HorizontalAccuracy) meter"
-            Tidstämpel = $Watcher.Position.Timestamp
         } | Format-List
     }
 }
-
 $Watcher.Stop()
+
+function print($text) {
+    Write-Host $text
+}
+print("creator is fitzypopper")
